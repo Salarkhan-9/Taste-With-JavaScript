b794dfc03ef75f284b3d6384f959d62184ac209a
Muhammad Salar Khan Marwat <98265148+Salarkhan-9@users.noreply.github.com>
2024-02-20 10:54:22 +0000

    objects

---
+++ 2_basic/3_objects.js
@@ -0,0 +1,24 @@
+//Objects 
+//literal ways ,singleton ,constructor
+//singleton is made form constructor 
+
+
+//object literals 
+
+
+Object.create //contructor --like this single ton is made 
+
+const JsUser={
+
+ name:'Salar',
+ "Full Name":"Salar khan",
+ age:23,
+ location:'Abbottabad',
+ email:'salarkhan842@gmail.com',
+ isloggedIn:false,
+ lastloginDays:['monday', 'saturday']
+
+}
+console.log(JsUser."Full Name")
+console.log(JsUser."Full Name")
+console.log(JsUser."Full Name")
