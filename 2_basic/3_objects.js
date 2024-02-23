52b62b34c583645c55021a1f66dfb298b8f68109
Muhammad Salar Khan Marwat <98265148+Salarkhan-9@users.noreply.github.com>
2024-02-23 17:40:21 +0000

    function add in obejct

--- 2_basic/3_objects.js
+++ 2_basic/3_objects.js
@@ -15,16 +15,17 @@ const JsUser={
  name:'Salar',
  "Full Name":"Salar khan",
  age:23,
- mySym:"MyKey1",
+ [mySym]:"MyKey1", //this square brackets ,if we want symbole data type of it instead of string . 
  location:'Abbottabad',
  email:'salarkhan842@gmail.com',
  isloggedIn:false,
  lastloginDays:['monday', 'saturday']
 
 }
+Object.freeze(JsUser)//this will freese the object
 
-
-console.log(JsUser."Full Name")
+// console.log(JsUser."Full Name")
 console.log(JsUser.emial)
 console.log(JsUser.["email"])
 console.log(JsUser["Full Name"])
+console.log(JsUser.mySym)//this will give my key1 =but its datatype is string it dont acts as symbole
